85703460c8e87052bce7f9a5a1ba4641859f9f1c
Mercedes Vázquez García <a01655224@itesm.mx>
2019-02-22 14:06:46 -0600

    Commit inicial

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _jumpForce = 5.0f;
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private bool resetJump = false;
+    [SerializeField] private GameObject paintDrop;
+    [SerializeField] private float fireRate = 0.25f;
+    private float canFire = 0f;
+    public bool havebrush = false;
+    private bool rigth = true;
+    
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private Rigidbody2D _rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Attack();
+    }
+
+    private void Movement()
+    {
+        //move the player
+        float move = Input.GetAxisRaw("Horizontal") * _speed;
+        if (move > 0)
+        {
+            sprite.flipX = false;
+            rigth = true;
+        }else if(move < 0)
+        {
+            sprite.flipX = true;
+            rigth = false;
+        }
+        transform.Translate(Vector3.right * move * _speed * Time.deltaTime);
+        MoveAnimation(move);
+        
+        //make the player jump
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            isGrounded = false;
+            resetJump = true;
+            StartCoroutine(ResetJumpCorutine());
+            Jump(true);
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 3.5f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down * 3.5f, Color.green);
+
+        if(hitInfo.collider != null && !resetJump)
+        {
+            Jump(false);
+            isGrounded = true;
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            shoot();
+        }
+    }
+
+    private void shoot()
+    {
+        if (havebrush)
+        {
+            if (Time.time > canFire)
+            {
+                PincelAttackAnim(true);
+                if(rigth)
+                    Instantiate(paintDrop, transform.position + new Vector3(2.42f, 0.89f, 0), Quaternion.Euler(0, 0, 90f));
+                if(!rigth)
+                    Instantiate(paintDrop, transform.position + new Vector3(-2.42f, 0.89f, 0), Quaternion.Euler(0, 0, -90f));
+                canFire = Time.time + fireRate;
+            }
+
+            StartCoroutine(WaitForShoot());
+        }
+    }
+
+    IEnumerator WaitForShoot()
+    {
+        yield return new WaitForSeconds(0.5f);
+        PincelAttackAnim(false);
+    }
+
+    IEnumerator ResetJumpCorutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        resetJump = false;
+    }
+
+    public void MoveAnimation(float move)
+    {
+        anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void Jump(bool jump)
+    {
+        anim.SetBool("Jump", jump);
+    }
+
+    public void PincelAttackAnim(bool attack)
+    {
+        anim.SetBool("Attack", attack);
+    }
+}

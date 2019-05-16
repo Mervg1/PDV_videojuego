447e752707439fd2e9f8841eedcbd2941bc00eeb
Gerardo N <dgnvrrg@gmail.com>
2019-05-15 19:41:20 -0500

    second re-upload

---
+++ Assets/_Scripts/Enemy/M_inion.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_inion : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform rigthP, leftP;
+    [SerializeField] private float speed = 2.0f;
+
+    private Vector3 _currentTarget;
+    private BulletEnemy bullet;
+    private SpriteRenderer _sprite;
+    private Animator _anim;
+
+    private bool canShoot = true;
+    private bool right = false;
+    private bool canMove = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
+        bullet = bulletPrefab.GetComponent<BulletEnemy>();
+        _currentTarget = rigthP.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shoot();
+    }
+
+    void Movement()
+    {
+        if (_currentTarget == rigthP.position)
+        {
+            _sprite.flipX = false;
+        }
+        else
+        {
+            _sprite.flipX = true;
+        }
+
+        if ((transform.position.x == rigthP.position.x) || (transform.position.x == leftP.position.x))
+        {
+            MoveAnimation(false);
+            canMove = false;
+            StartCoroutine(waitToWalk());
+        }
+
+        if (canMove)
+        {
+            MoveAnimation(true);
+            transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+        }
+    }
+
+    private void Shoot()
+    {
+        if (canShoot)
+        {   
+            if (_currentTarget == leftP.position)
+                Instantiate(bulletPrefab, transform.position + new Vector3(-1.2f, 0.8f, 0), Quaternion.Euler(0f, 0f, -90f));
+            if (_currentTarget == rigthP.position)
+                Instantiate(bulletPrefab, transform.position + new Vector3(1.2f, 0.8f, 0), Quaternion.Euler(0f, 0f, 90f));
+            canShoot = false;
+            StartCoroutine(waitForShoot());
+        }
+    }
+
+    IEnumerator waitForShoot()
+    {
+        yield return new WaitForSeconds(3f);
+        canShoot = true;
+    }
+
+    IEnumerator waitToWalk()
+    {
+        yield return new WaitForSeconds(1.5f);
+        canMove = true;
+        if (transform.position.x == rigthP.position.x)
+        {
+            _currentTarget = leftP.position;
+        }
+        if (transform.position.x == leftP.position.x)
+        {
+            _currentTarget = rigthP.position;
+        }
+    }
+
+    public void MoveAnimation(bool move)
+    {
+        _anim.SetBool("canMove", move);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Bullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/Props/Brush.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush : MonoBehaviour
+{
+    private float up, down;
+    private bool upDown = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = transform.position.y + 0.2f;
+        down = transform.position.y - 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (transform.position.y <= down)
+        {
+            upDown = false;
+        }
+
+        if (transform.position.y >= up)
+        {
+            upDown = true;
+        }
+
+        if (upDown)
+            transform.Translate(Vector3.down * 0.25f * Time.deltaTime);
+        if (!upDown)
+            transform.Translate(Vector3.up * 0.25f * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Player player = collision.GetComponent<Player>();
+            if(player != null)
+            {
+                player.havebrush = true;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}

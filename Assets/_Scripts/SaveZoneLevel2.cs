4135b4879886e33b83793f21ed9bc79295b572bb
Gerardo N <dgnvrrg@gmail.com>
2019-05-17 12:56:11 -0500

    final

---
+++ Assets/_Scripts/SaveZoneLevel2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveZoneLevel2 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Player player = collision.GetComponent<Player>();
+            if(player != null)
+            {
+                player.transform.position = new Vector3(-64f, 0, 0);
+            }
+        }
+    }
+}
---
+++ Assets/saveZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saveZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                player.transform.position = new Vector3(590, 16, 0);
+            }
+        }
+    }
+}

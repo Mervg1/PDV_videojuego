789aa4cc32f0d525fe999ac2f27592040d332409
Gerardo N <dgnvrrg@gmail.com>
2019-05-17 04:44:50 -0500

    tercer nivel, faltan detalles

---
+++ Assets/_Scripts/EspecialScripts/King_Gray.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King_Gray : MonoBehaviour
+{
+    private float up, down;
+    private bool upDown = true;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float fTransition = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = transform.position.y + fTransition;
+        down = transform.position.y - fTransition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (transform.position.y <= down)
+        {
+            upDown = false;
+        }
+
+        if (transform.position.y >= up)
+        {
+            upDown = true;
+        }
+
+        if (upDown)
+            transform.Translate(Vector3.down * 0.25f * Time.deltaTime* speed);
+        if (!upDown)
+            transform.Translate(Vector3.up * 0.25f * Time.deltaTime * speed);
+    }
+}
